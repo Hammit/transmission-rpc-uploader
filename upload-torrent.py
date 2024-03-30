df41584486041366775f8edd01dcab4577bbfdaf
Byron Hammond <byron_hammond@yahoo.com.au>
2024-03-30 18:48:11 +0800

    Added logging

--- upload-torrent.py
+++ upload-torrent.py
@@ -2,6 +2,7 @@
 #
 # venv/bin/python3 upload-torrent.py /path/to/filename.torrent
 
+import logging
 import os
 import sys
 
@@ -16,6 +17,10 @@ if len(sys.argv) == 1:
 
 torrent_filename = sys.argv[1]
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(filename=f'{os.path.basename(__file__)}.log', level=logging.INFO)
+
+logger.info('Loading environment variables from .env')
 load_dotenv()
 
 client = Client(
@@ -25,5 +30,7 @@ client = Client(
     password=os.getenv('PASSWORD')
 )
 with open(torrent_filename, "rb") as f:
+    logger.info(f'Adding {torrent_filename} to remote transmission daemon via RPC')
     client.add_torrent(f)
+logger.info('Done')
 
