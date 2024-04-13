a91fb034bd1021da512b6d1720814f09b70190ac
Byron Hammond <byron_hammond@yahoo.com.au>
2024-04-13 11:52:07 +0800

    Added some comments

--- upload-torrent.py
+++ upload-torrent.py
@@ -18,6 +18,7 @@ if len(sys.argv) == 1:
 
 torrent_filename = sys.argv[1]
 
+# Setup logging
 logger = logging.getLogger(__name__)
 script_dir = sys.path[0]
 script_filename = os.path.basename(__file__)
@@ -25,15 +26,18 @@ script_basename = os.path.splitext(script_filename)[0]
 log_filename = f'{script_basename}.log'
 logging.basicConfig(filename=f'{script_dir}/{log_filename}', level=logging.INFO)
 
+# Load config/secrets
 logger.info('Loading environment variables from .env')
 load_dotenv()
 
+# Create the Transmission Client that does the RPC
 client = Client(
     host=os.getenv('HOST', ''),
     port=int(os.getenv('PORT', DEFAULT_TRANSMISSION_PORT)),
     username=os.getenv('USERNAME'),
     password=os.getenv('PASSWORD')
 )
+# Send the torrent
 with open(torrent_filename, "rb") as f:
     logger.info(f'Adding {torrent_filename} to remote transmission daemon via RPC')
     client.add_torrent(f)
